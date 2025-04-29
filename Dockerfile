97a019814a4e4910f4ed0a276f7be35f8e63e19f
Heitor <heitorhenrique2003@gmail.com>
2025-04-29 10:00:07 -0300

    :bug: fix: Correçãoo de erro do docker

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,9 @@ RUN apt-get update && apt-get install -y unzip libpq-dev netcat-openbsd && docke
 WORKDIR /var/www
 
 COPY --from=vitebuilder /app /var/www
-COPY entrypoint.sh /entrypoint.sh
 
-RUN chmod +x /entrypoint.sh && chown -R www-data:www-data /var/www
-
-EXPOSE 9000
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+RUN chmod +x /usr/local/bin/entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
+CMD [ "php-fpm" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# carrega as variáveis de ambiente
+source /var/www/.env
+
 # Espera o banco ficar disponível antes de migrar
 echo "Aguardando o banco de dados subir..."
-until nc -z $DB_HOST $DB_PORT; do
-  sleep 2
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
 done
+echo "Banco de dados disponível!"
+
+cd /var/www
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+composer install --no-dev --optimize-autoloader
+
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www
 
 # Roda as migrations
 php artisan migrate --force
-
-# Inicia o servidor php-fpm ou outro processo principal
-php-fpm
-    
